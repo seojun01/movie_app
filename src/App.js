82cbd263a56e7cd3b97f03dc448661b6964b3338
seojun01 <parkseojoon@naver.com>
2023-04-05 21:07:37 +0100

    Coin Tracker

--- src/App.js
+++ src/App.js
@@ -1,37 +1,12 @@
 import React, { useState } from "react";
+import ToDo from './toDo';
+import CoinTracker from './coinTracker';
 
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event) => {
-    setToDo(event.target.value);
-  }
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if(toDo === "") {
-      return;
-    }
-    setToDo("");
-    setToDos((currentArray) => [toDo, ...currentArray]);
-  }
   return (
-    <div>
-      <h1>My To Dos ({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input 
-          onChange={onChange} 
-          value={toDo} 
-          type="text" 
-          placeholder="Write your to do.." 
-        />
-        <button>Add To Do</button>
-      </form>
-      <hr />
-      <ul>
-        {toDos.map((item, index) => <li key={index}>{item}</li>)}
-      </ul>
-    </div>
-  );
+    // <ToDo />
+    <CoinTracker />
+  )
 }
 
 export default App;
